a48a1d95b04d52d08529c036f5a9b9e815c649fd
Damyan Doykov <strikerbg98@gmail.com>
2025-12-07 11:30:13 +0200

    Update index name in connect_db.py to "isi-data-hybrid"

--- connect_db.py
+++ connect_db.py
@@ -5,15 +5,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-index_name = "isi-data-test2"
+index_name = "isi-data-hybrid"
 
-# initialize Pinecone client
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 if index_name not in pc.list_indexes().names():
     pc.create_index(
         name=index_name,
-        dimension=3072,  # <-- change to your embedding model
+        dimension=3072,  
         metric="dotproduct",
         spec=ServerlessSpec(cloud="aws", region="us-east-1"),
     )
